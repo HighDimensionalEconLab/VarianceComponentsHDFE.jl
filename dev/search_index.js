6b193e8f8062af55644937fa114b041a0e7c2415
Documenter.jl <documenter@juliadocs.github.io>
2020-10-26 19:11:48 +0000

    build based on 34584c7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VarianceComponentsHDFE","category":"page"},{"location":"#VarianceComponentsHDFE","page":"Home","title":"VarianceComponentsHDFE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VarianceComponentsHDFE]","category":"page"}]
+}
