b1ef18be01f1cd35cddd4fdc1b71c97b09779346
paulcorcuera <paulcorcuera.g@gmail.com>
2020-10-26 11:13:35 -0700

    Update benchmarks
    Updating benchmark timings that now include the cluster timings (32 cores and 256 GB memory) . Also updates change in artifacts.toml directory.

--- benchmark/compute_whole.jl
+++ benchmark/compute_whole.jl
@@ -5,7 +5,7 @@ using ArtifactUtils, Pkg.Artifacts
 # Make sure LDLFactorizations is version 0.5.0 and that the `multiple-rhs` branch is checked out
 
 pkg_dir = pkgdir(VarianceComponentsHDFE)
-artifacts_path = joinpath(pkgdir(VarianceComponentsHDFE), "deps\\Artifacts.toml")
+artifacts_path = joinpath(pkgdir(VarianceComponentsHDFE), "benchmark\\Artifacts.toml")
 
 
 #Set Random seed
@@ -40,12 +40,19 @@ settings_JLA = VCHDFESettings(leverage_algorithm = JLAAlgorithm(num_simulations=
 
 settings_JLA = VCHDFESettings(leverage_algorithm = JLAAlgorithm(num_simulations=200), first_id_effects=true, cov_effects=true)
 
-@btime compute_whole($y,$id,$firmid,nothing,settings_JLA) #1702.217 ss Julia vs 1115.04s Matlab (32GB Ram and 4 Threads, 100 simulations)
+@btime compute_whole($y,$id,$firmid,nothing,settings_JLA) #1702.217 ss Julia vs 1115.04s Matlab (32GB Ram and 4 Threads, 200 simulations)
 
 
 settings_JLA = VCHDFESettings(leverage_algorithm = JLAAlgorithm(num_simulations=500), first_id_effects=true, cov_effects=true)
 
-@btime compute_whole(y,id,firmid,nothing,settings_JLA) # 4280.219173 s in Julia vs 3328.10s in Matlab
+@btime compute_whole(y,id,firmid,nothing,settings_JLA) # 4280.219173 s in Julia vs 3328.10s in Matlab (32GB Ram and 4 Threads, 500 simulations)
+
+
+#Simulations on cluster for 250 rademachers: 949.138618 seconds Julia vs 379.719083  (256GB and 32cores )
+#Simulations on cluster for 500 rademachers: 1463.095847 seconds Julia vs 628.135439 seconds Matlab (256GB and 32cores)
+#Simulations on cluster for heuristic - 1441 rademachers : 3392.993448 seconds Julia vs 1651.819884 seconds Matlab  (256GB and 32cores)
+
+
 
 
 # 3) Huge sized network benchmarks 
@@ -59,7 +66,6 @@ obs_id = collect(1:length(y))
 
 settings_JLA = VCHDFESettings(leverage_algorithm = JLAAlgorithm(num_simulations=100), first_id_effects=true, cov_effects=true)
 
-@btime compute_whole($y,$id,$firmid,nothing,settings_JLA) #s Julia vs s Matlab (32GB Ram and 4 Threads, 100 simulations)
 
 
 
