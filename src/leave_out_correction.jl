bf29369eaa33932fba0f42b67dcc05921318942f
paulcorcuera <paulcorcuera.g@gmail.com>
2020-11-02 01:32:54 -0800

    Update leave_out_correction.jl
    In verbose mode it now prints the number of JLA draws that you're running.

--- src/leave_out_correction.jl
+++ src/leave_out_correction.jl
@@ -726,6 +726,8 @@ function eff_res(lev::JLAAlgorithm, X,first_id,second_id,match_id, K, settings)
         Fvar= hcat(spzeros(NT,N), X[:,N+1:N+J-1])
         Dvar=hcat(X[:,1:N], spzeros(NT,J-1))
 
+        (settings.print_level > 0) && println("Running JLA with ",p," simulations.")
+
         Threads.@threads for i=1:p
             #Draw Rademacher entry
             rademach[Threads.threadid(),:] =  rand(1,NT) .> 0.5
