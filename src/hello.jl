bec46b00945f989b18007006ec84b0ac9c4c68d8
= <71057115+Alim-faraji@users.noreply.github.com>
2021-04-06 11:56:10 -0700

    converting LinOp to vector instead of a Matrix

--- src/hello.jl
+++ src/hello.jl
@@ -40,13 +40,13 @@ settings = VCHDFESettings(leverage_algorithm = JLAAlgorithm(num_simulations=0),
 
 print(size(data,1) == size(y, 1))
 
-using JLD2
-@save "tmp.jld2" obs y first_id second_id controls settings
-# @save "tmp2.jld2" obs y first_id second_id controls settings
-# @load "tmp.jld2" obs y first_id second_id controls y_raw first_id_raw second_id_raw settings
-@load "tmp3.jld2" obs y first_id second_id controls 
-# using LinearOperators
-# Dbarvar = hcat(F * opCholesky((F' *F)) * F' * D , spzeros(NT,J-1) )
+# using JLD2
+# @save "tmp.jld2" obs y first_id second_id controls settings
+# # @save "tmp2.jld2" obs y first_id second_id controls settings
+# # @load "tmp.jld2" obs y first_id second_id controls y_raw first_id_raw second_id_raw settings
+# @load "tmp3.jld2" obs y first_id second_id controls 
+# # using LinearOperators
+# # Dbarvar = hcat(F * opCholesky((F' *F)) * F' * D , spzeros(NT,J-1) )
 
 @time @unpack θ_first, θ_second, θCOV, θ_firstbar, β, Dalpha, Fpsi, Pii, Bii_first, Bii_second, Bii_cov, Bii_first_bar, y, X, sigma_i = leave_out_estimation(y,first_id,second_id,controls,settings)
 
