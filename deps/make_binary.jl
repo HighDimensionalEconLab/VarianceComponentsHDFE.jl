eb4a73f14dc9228f1c4ae69bd79c5c6096768a0e
= <71057115+Alim-faraji@users.noreply.github.com>
2021-05-25 17:56:19 -0700

    TimeZones.jl issue. turningoff lazy artifacts

--- deps/make_binary.jl
+++ deps/make_binary.jl
@@ -1,2 +1,2 @@
 using PackageCompiler
-create_app(".", "./vchdfe", app_name="vchdfe", force=true, precompile_execution_file="deps/precompilation_execution_file.jl")
+create_app(".", "./vchdfe", app_name="vchdfe", force=true, precompile_execution_file="deps/precompilation_execution_file.jl", include_lazy_artifacts = false)
