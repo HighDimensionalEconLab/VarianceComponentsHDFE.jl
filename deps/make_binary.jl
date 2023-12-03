0882d035557f2a4f424650e86813a439fa4f1fe9
Paul Corcuera <paulcorcuera.g@gmail.com>
2023-12-02 19:05:46 -0800

    Update make_binary.jl
    Update to packagecompiler v2

--- deps/make_binary.jl
+++ deps/make_binary.jl
@@ -1,2 +1,2 @@
 using PackageCompiler
-create_app(".", "./vchdfe", app_name="vchdfe", force=true, precompile_execution_file="deps/precompilation_execution_file.jl", include_lazy_artifacts=true)
+create_app(".", "./vchdfe", executables=["vchdfe"=>"julia_main"], force=true, precompile_execution_file="deps/precompilation_execution_file.jl", include_lazy_artifacts=true)
